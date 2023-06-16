836aabfc30938c536b106e8e6857234f5b9db6d0
nagarjun73 <nagarjun7397@gmail.com>
2023-06-16 11:26:42 +0530

    assaignment set

--- JavaScript/Assignments/JS SET 4 Assignments/3.js
+++ JavaScript/Assignments/JS SET 4 Assignments/3.js
@@ -1,16 +1,20 @@
 function joinWords(a) {
-    let result = a[0]
-    let initialSplice = []
-    let commonWord = []
-    let wordCommon = ''
+    let result = a[0] //oven + vieraserious
+    let initialSplice = [] //['oven','ven','en','n','envier', 'envie',envi','env','en','e']
+    let commonWord = [] // ['oven','ven','en','n','envier','envie','envi','env','e']
+    let wordCommon = '' // en
+
+    //Slicing first word
   for(let i=0; i<a[0].length; i++){
       initialSplice.push(a[0].slice(i))
     }
 
+    //Slicing 2nd word
   for(let b=0; b<a[1].length; b++){
       initialSplice.push(a[1].slice(0, a[1].length-b))
   }
 
+  //Finding common word
   for(let j=0; j<initialSplice.length; j++){
     if(!commonWord.includes(initialSplice[j])){
       commonWord.push(initialSplice[j])
@@ -19,11 +23,12 @@ function joinWords(a) {
     }
   }
 
+  //Adding words based on common words
   for(let d=1; d<a.length; d++){
     let nxtVal = a[d]
     result += nxtVal.slice(wordCommon.length)
   }
-  return  result
+  return  wordCommon
 }
 
 console.log(joinWords(["aaa", "bbb", "ccc", "ddd"]));

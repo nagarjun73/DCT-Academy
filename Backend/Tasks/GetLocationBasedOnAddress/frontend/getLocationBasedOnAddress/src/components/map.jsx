1191509f22522e84523364506bde5766918bff05
nagarjun73 <nagarjun7397@gmail.com>
2023-09-07 22:38:33 +0530

    added map fly feature when different city selected

--- Backend/Tasks/GetLocationBasedOnAddress/frontend/getLocationBasedOnAddress/src/components/map.jsx
+++ Backend/Tasks/GetLocationBasedOnAddress/frontend/getLocationBasedOnAddress/src/components/map.jsx
@@ -1,14 +1,30 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { PlacesContext } from '../App'
 
 export default function MapComp(props) {
   const { places } = useContext(PlacesContext)
+  const [positions, setPositions] = useState([12.9457315, 77.5717632])
+
+  //custom function to change map view based on change in positions
+  function ChangeMapView({ coords }) {
+    const map = useMap();
+    map.flyTo(coords, map.getZoom())
+    // map.setView(coords, map.getZoom());
+    return null;
+  }
+
+  //to trigger when selected coords updated so that the map center changes
+  useEffect(() => {
+    if (places.selectedCity.length) {
+      setPositions(places.selectedCity)
+    }
+  }, [places.selectedCity])
 
   return (
     <div>
-      <MapContainer style={{ height: "400px" }} center={places.selectedCity.length ? places.selectedCity : [12.9457315, 77.5717632]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer style={{ height: "400px", width: '100%' }} center={positions} zoom={11} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -28,6 +44,8 @@ export default function MapComp(props) {
               </Popup>
             </Marker>
           })}
+          //To change center map position
+        <ChangeMapView coords={positions} />
       </MapContainer>
     </div>
   )

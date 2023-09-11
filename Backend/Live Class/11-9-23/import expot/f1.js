6d948acae3405efed4da0748ae51c5c2c89cd4d6
nagarjun73 <nagarjun7397@gmail.com>
2023-09-11 10:50:03 +0530

    class 11-9-23

---
+++ Backend/Live Class/11-9-23/import expot/f1.js
@@ -0,0 +1,5 @@
+const { firstName, city } = require("./f3")
+const greet = require('./f2')
+
+// import { firstName, city } from "./f3"
+// import greet from "./f2"
---
+++ Backend/Live Class/11-9-23/import expot/f3.js
@@ -0,0 +1,10 @@
+const firstName = "adam"
+const city = "banglore"
+
+module.exports = {
+  firstName,
+  city
+}
+
+// export const firstName = "adam"
+// export const city = "banglore"
